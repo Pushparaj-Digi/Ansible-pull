da368442eeb0a5cb0b4f9fc4f60525acc583f1c5
Pushparaj <crismacris2000@gmail.com>
2025-12-01 23:14:50 +0530

    Updated fix

--- agent/install.sh
+++ agent/install.sh
@@ -1,24 +1,60 @@
 #!/bin/bash
 
-# ====== CONFIG ======
-AWX_TOKEN="tYijIihFBmNPMPxHYrM1jQWNDXkL9L"
+
+
+### CONFIG ###
+
 AWX_URL="http://192.168.1.11:30080"
+
+AWX_TOKEN="tYijIihFBmNPMPxHYrM1jQWNDXkL9L"
+
 INVENTORY_ID="2"
+
 REGION="default"
 
+
+
 echo "Installing dependencies..."
+
 sudo apt-get update -y
+
 sudo apt-get install -y ansible git curl jq
 
+
+
 echo "Collecting client details..."
+
 HOSTNAME=$(hostname)
+
 IP=$(hostname -I | awk '{print $1}')
+
 SERIAL=$(cat /proc/cpuinfo | grep Serial | awk '{print $3}')
+
+
 
 echo "Registering client $HOSTNAME to AWX..."
 
-curl -s -X POST "$AWX_URL/api/v2/hosts/" -H "Authorization: Bearer $AWX_TOKEN" -H "Content-Type: application/json" \
--d "{\"name\":\"$HOSTNAME\",\"description\":\"Auto registered client\",\"inventory\":$INVENTORY_ID,\"variables\":\"ip: $IP\nserial: $SERIAL\nregion: $REGION\"}"
+
+
+curl -s -X POST "$AWX_URL/api/v2/hosts/" \
+
+-H "Authorization: Bearer $AWX_TOKEN" \
+
+-H "Content-Type: application/json" \
+
+-d "{\"name\": \"$HOSTNAME\", \"inventory\": $INVENTORY_ID, \"description\": \"Auto added client\", \"variables\": \"ip: $IP\nserial: $SERIAL\nregion: $REGION\"}"
+
+
 
 echo "Running registration playbook..."
-ansible-pull -U https://github.com/Pushparaj-Digi/Ansible-pull.git playbooks/register_client.yml
+
+
+
+ansible-pull \
+
+  -U https://github.com/Pushparaj-Digi/Ansible-pull.git \
+
+  playbooks/register_client.yml \
+
+  --extra-vars "awx_token=$AWX_TOKEN awx_url=$AWX_URL inventory_id=$INVENTORY_ID hostname=$HOSTNAME"
+
