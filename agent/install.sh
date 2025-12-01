5484b868a2e87a17457fb1948145c3288ec7c495
Pushparaj <crismacris2000@gmail.com>
2025-12-01 22:42:07 +0530

    Added AWX auto-registration to script

--- agent/install.sh
+++ agent/install.sh
@@ -2,21 +2,49 @@
 
 
 
-AWX_TOKEN="tYijIihFBmNPMPxHYrM1jQWNDXkL9L"
+AWX_URL="http://192.168.1.11:30080/api/v2/hosts/"
+
+AWX_TOKEN="lXnGYSOrOYNqrKQTZ3P2rZRDQdVRbs"
+
+INVENTORY_ID="2"
 
 
 
-sudo apt update -y
+HOSTNAME=$(hostname)
 
-sudo apt install -y ansible git curl
+IP=$(hostname -I | awk '{print $1}')
+
+SERIAL=$(cat /proc/cpuinfo | grep Serial | awk '{print $3}')
 
 
+
+echo "Registering client $HOSTNAME to AWX..."
+
+
+
+curl -k -s -X POST "$AWX_URL" \
+
+  -H "Content-Type: application/json" \
+
+  -H "Authorization: Bearer $AWX_TOKEN" \
+
+  -d "{
+
+      \"name\": \"$HOSTNAME\",
+
+      \"description\": \"IP:$IP Serial:$SERIAL\",
+
+      \"inventory\": \"$INVENTORY_ID\"
+
+  }"
+
+
+
+echo "Running registration playbook..."
 
 ansible-pull \
 
   -U https://github.com/Pushparaj-Digi/Ansible-pull.git \
 
-  playbooks/register_client.yml \
-
-  -e awx_token=$AWX_TOKEN
+  playbooks/register_client.yml
 
