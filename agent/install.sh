86d041f346837abcecafbac4df2efd901ed04715
Pushparaj <crismacris2000@gmail.com>
2025-12-01 23:18:03 +0530

    Fix new

--- agent/install.sh
+++ agent/install.sh
@@ -28,7 +28,7 @@ HOSTNAME=$(hostname)
 
 IP=$(hostname -I | awk '{print $1}')
 
-SERIAL=$(cat /proc/cpuinfo | grep Serial | awk '{print $3}')
+SERIAL=$(grep Serial /proc/cpuinfo | awk '{print $3}')
 
 
 
@@ -38,15 +38,15 @@ echo "Registering client $HOSTNAME to AWX..."
 
 curl -s -X POST "$AWX_URL/api/v2/hosts/" \
 
--H "Authorization: Bearer $AWX_TOKEN" \
+    -H "Authorization: Bearer $AWX_TOKEN" \
 
--H "Content-Type: application/json" \
+    -H "Content-Type: application/json" \
 
--d "{\"name\": \"$HOSTNAME\", \"inventory\": $INVENTORY_ID, \"description\": \"Auto added client\", \"variables\": \"ip: $IP\nserial: $SERIAL\nregion: $REGION\"}"
+    -d "{\"name\":\"$HOSTNAME\",\"inventory\":$INVENTORY_ID,\"description\":\"Auto added client\",\"variables\":\"ip: $IP\nserial: $SERIAL\nregion: $REGION\"}"
 
 
 
-echo "Running registration playbook..."
+echo "Running registration playbook with ansible-pull..."
 
 
 
